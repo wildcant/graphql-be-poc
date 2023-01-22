338373a491a1ae16fe742758483b515e2bc3cdc0
Wilmer Cantillo <will.canti2697@gmail.com>
2023-01-22 16:06:34 -0500

    datasources
    rename

--- src/modules/graphql-types.ts
+++ src/modules/graphql-types.ts
@@ -6,134 +6,134 @@
 
 /* eslint-disable */
 
-import { GraphQLResolveInfo } from "graphql"
-import { BookModel, AuthorModel, LibraryModel } from "../datasources"
-import { IContext } from "../context"
-export type Maybe<T> = T | null
-export type InputMaybe<T> = Maybe<T>
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] }
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> }
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> }
-export type RequireFields<T, K extends keyof T> = Omit<T, K> & { [P in K]-?: NonNullable<T[P]> }
+import { GraphQLResolveInfo } from 'graphql';
+import { BookModel, AuthorModel, LibraryModel } from '../datasources';
+import { IContext } from '../context';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type RequireFields<T, K extends keyof T> = Omit<T, K> & { [P in K]-?: NonNullable<T[P]> };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: string
-  String: string
-  Boolean: boolean
-  Int: number
-  Float: number
-}
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
 
 export type Author = {
-  __typename?: "Author"
-  id: Scalars["Int"]
-  name?: Maybe<Scalars["String"]>
-}
+  __typename?: 'Author';
+  id: Scalars['Int'];
+  name?: Maybe<Scalars['String']>;
+};
 
 export type Book = {
-  __typename?: "Book"
-  author?: Maybe<Author>
-  computedProperty?: Maybe<Scalars["Int"]>
-  computedPropertyDoubled?: Maybe<Scalars["Int"]>
-  editorial?: Maybe<Editorial>
-  id: Scalars["Int"]
-  title?: Maybe<Scalars["String"]>
-}
+  __typename?: 'Book';
+  author?: Maybe<Author>;
+  computedProperty?: Maybe<Scalars['Int']>;
+  computedPropertyDoubled?: Maybe<Scalars['Int']>;
+  editorial?: Maybe<Editorial>;
+  id: Scalars['Int'];
+  title?: Maybe<Scalars['String']>;
+};
 
 export type Editorial = {
-  __typename?: "Editorial"
-  name?: Maybe<Scalars["String"]>
-}
+  __typename?: 'Editorial';
+  name?: Maybe<Scalars['String']>;
+};
 
 export type Library = {
-  __typename?: "Library"
-  address?: Maybe<Scalars["String"]>
-  books?: Maybe<Array<Maybe<Book>>>
-  id: Scalars["String"]
-  name?: Maybe<Scalars["String"]>
-}
+  __typename?: 'Library';
+  address?: Maybe<Scalars['String']>;
+  books?: Maybe<Array<Maybe<Book>>>;
+  id: Scalars['String'];
+  name?: Maybe<Scalars['String']>;
+};
 
 export type Query = {
-  __typename?: "Query"
-  author?: Maybe<Author>
-  book?: Maybe<Book>
-  books?: Maybe<Array<Maybe<Book>>>
-  library?: Maybe<Library>
-}
+  __typename?: 'Query';
+  author?: Maybe<Author>;
+  book?: Maybe<Book>;
+  books?: Maybe<Array<Maybe<Book>>>;
+  library?: Maybe<Library>;
+};
+
 
 export type QueryAuthorArgs = {
-  id: Scalars["Int"]
-}
+  id: Scalars['Int'];
+};
+
 
 export type QueryBookArgs = {
-  id: Scalars["Int"]
-}
+  id: Scalars['Int'];
+};
+
 
 export type QueryLibraryArgs = {
-  id: Scalars["String"]
-}
+  id: Scalars['String'];
+};
 
-export type ResolverTypeWrapper<T> = Promise<T> | T
+
+
+export type ResolverTypeWrapper<T> = Promise<T> | T;
+
 
 export type ResolverWithResolve<TResult, TParent, TContext, TArgs> = {
-  resolve: ResolverFn<TResult, TParent, TContext, TArgs>
-}
-export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> =
-  | ResolverFn<TResult, TParent, TContext, TArgs>
-  | ResolverWithResolve<TResult, TParent, TContext, TArgs>
+  resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
+};
+export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> = ResolverFn<TResult, TParent, TContext, TArgs> | ResolverWithResolve<TResult, TParent, TContext, TArgs>;
 
 export type ResolverFn<TResult, TParent, TContext, TArgs> = (
   parent: TParent,
   args: TArgs,
   context: TContext,
   info: GraphQLResolveInfo
-) => Promise<TResult> | TResult
+) => Promise<TResult> | TResult;
 
 export type SubscriptionSubscribeFn<TResult, TParent, TContext, TArgs> = (
   parent: TParent,
   args: TArgs,
   context: TContext,
   info: GraphQLResolveInfo
-) => AsyncIterable<TResult> | Promise<AsyncIterable<TResult>>
+) => AsyncIterable<TResult> | Promise<AsyncIterable<TResult>>;
 
 export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
   parent: TParent,
   args: TArgs,
   context: TContext,
   info: GraphQLResolveInfo
-) => TResult | Promise<TResult>
+) => TResult | Promise<TResult>;
 
 export interface SubscriptionSubscriberObject<TResult, TKey extends string, TParent, TContext, TArgs> {
-  subscribe: SubscriptionSubscribeFn<{ [key in TKey]: TResult }, TParent, TContext, TArgs>
-  resolve?: SubscriptionResolveFn<TResult, { [key in TKey]: TResult }, TContext, TArgs>
+  subscribe: SubscriptionSubscribeFn<{ [key in TKey]: TResult }, TParent, TContext, TArgs>;
+  resolve?: SubscriptionResolveFn<TResult, { [key in TKey]: TResult }, TContext, TArgs>;
 }
 
 export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
-  subscribe: SubscriptionSubscribeFn<any, TParent, TContext, TArgs>
-  resolve: SubscriptionResolveFn<TResult, any, TContext, TArgs>
+  subscribe: SubscriptionSubscribeFn<any, TParent, TContext, TArgs>;
+  resolve: SubscriptionResolveFn<TResult, any, TContext, TArgs>;
 }
 
 export type SubscriptionObject<TResult, TKey extends string, TParent, TContext, TArgs> =
   | SubscriptionSubscriberObject<TResult, TKey, TParent, TContext, TArgs>
-  | SubscriptionResolverObject<TResult, TParent, TContext, TArgs>
+  | SubscriptionResolverObject<TResult, TParent, TContext, TArgs>;
 
 export type SubscriptionResolver<TResult, TKey extends string, TParent = {}, TContext = {}, TArgs = {}> =
   | ((...args: any[]) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
-  | SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>
+  | SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>;
 
 export type TypeResolveFn<TTypes, TParent = {}, TContext = {}> = (
   parent: TParent,
   context: TContext,
   info: GraphQLResolveInfo
-) => Maybe<TTypes> | Promise<Maybe<TTypes>>
+) => Maybe<TTypes> | Promise<Maybe<TTypes>>;
 
-export type IsTypeOfResolverFn<T = {}, TContext = {}> = (
-  obj: T,
-  context: TContext,
-  info: GraphQLResolveInfo
-) => boolean | Promise<boolean>
+export type IsTypeOfResolverFn<T = {}, TContext = {}> = (obj: T, context: TContext, info: GraphQLResolveInfo) => boolean | Promise<boolean>;
 
-export type NextResolverFn<T> = () => Promise<T>
+export type NextResolverFn<T> = () => Promise<T>;
 
 export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs = {}> = (
   next: NextResolverFn<TResult>,
@@ -141,87 +141,73 @@ export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs
   args: TArgs,
   context: TContext,
   info: GraphQLResolveInfo
-) => TResult | Promise<TResult>
+) => TResult | Promise<TResult>;
 
 /** Mapping between all available schema types and the resolvers types */
 export type ResolversTypes = {
-  Author: ResolverTypeWrapper<AuthorModel>
-  Book: ResolverTypeWrapper<BookModel>
-  Boolean: ResolverTypeWrapper<Scalars["Boolean"]>
-  Editorial: ResolverTypeWrapper<Editorial>
-  Int: ResolverTypeWrapper<Scalars["Int"]>
-  Library: ResolverTypeWrapper<LibraryModel>
-  Query: ResolverTypeWrapper<{}>
-  String: ResolverTypeWrapper<Scalars["String"]>
-}
+  Author: ResolverTypeWrapper<AuthorModel>;
+  Book: ResolverTypeWrapper<BookModel>;
+  Boolean: ResolverTypeWrapper<Scalars['Boolean']>;
+  Editorial: ResolverTypeWrapper<Editorial>;
+  Int: ResolverTypeWrapper<Scalars['Int']>;
+  Library: ResolverTypeWrapper<LibraryModel>;
+  Query: ResolverTypeWrapper<{}>;
+  String: ResolverTypeWrapper<Scalars['String']>;
+};
 
 /** Mapping between all available schema types and the resolvers parents */
 export type ResolversParentTypes = {
-  Author: AuthorModel
-  Book: BookModel
-  Boolean: Scalars["Boolean"]
-  Editorial: Editorial
-  Int: Scalars["Int"]
-  Library: LibraryModel
-  Query: {}
-  String: Scalars["String"]
-}
+  Author: AuthorModel;
+  Book: BookModel;
+  Boolean: Scalars['Boolean'];
+  Editorial: Editorial;
+  Int: Scalars['Int'];
+  Library: LibraryModel;
+  Query: {};
+  String: Scalars['String'];
+};
 
-export type AuthorResolvers<
-  ContextType = IContext,
-  ParentType extends ResolversParentTypes["Author"] = ResolversParentTypes["Author"]
-> = {
-  id?: Resolver<ResolversTypes["Int"], ParentType, ContextType>
-  name?: Resolver<Maybe<ResolversTypes["String"]>, ParentType, ContextType>
-  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>
-}
+export type AuthorResolvers<ContextType = IContext, ParentType extends ResolversParentTypes['Author'] = ResolversParentTypes['Author']> = {
+  id?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  name?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
 
-export type BookResolvers<
-  ContextType = IContext,
-  ParentType extends ResolversParentTypes["Book"] = ResolversParentTypes["Book"]
-> = {
-  author?: Resolver<Maybe<ResolversTypes["Author"]>, ParentType, ContextType>
-  computedProperty?: Resolver<Maybe<ResolversTypes["Int"]>, ParentType, ContextType>
-  computedPropertyDoubled?: Resolver<Maybe<ResolversTypes["Int"]>, ParentType, ContextType>
-  editorial?: Resolver<Maybe<ResolversTypes["Editorial"]>, ParentType, ContextType>
-  id?: Resolver<ResolversTypes["Int"], ParentType, ContextType>
-  title?: Resolver<Maybe<ResolversTypes["String"]>, ParentType, ContextType>
-  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>
-}
+export type BookResolvers<ContextType = IContext, ParentType extends ResolversParentTypes['Book'] = ResolversParentTypes['Book']> = {
+  author?: Resolver<Maybe<ResolversTypes['Author']>, ParentType, ContextType>;
+  computedProperty?: Resolver<Maybe<ResolversTypes['Int']>, ParentType, ContextType>;
+  computedPropertyDoubled?: Resolver<Maybe<ResolversTypes['Int']>, ParentType, ContextType>;
+  editorial?: Resolver<Maybe<ResolversTypes['Editorial']>, ParentType, ContextType>;
+  id?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+  title?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
 
-export type EditorialResolvers<
-  ContextType = IContext,
-  ParentType extends ResolversParentTypes["Editorial"] = ResolversParentTypes["Editorial"]
-> = {
-  name?: Resolver<Maybe<ResolversTypes["String"]>, ParentType, ContextType>
-  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>
-}
+export type EditorialResolvers<ContextType = IContext, ParentType extends ResolversParentTypes['Editorial'] = ResolversParentTypes['Editorial']> = {
+  name?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
 
-export type LibraryResolvers<
-  ContextType = IContext,
-  ParentType extends ResolversParentTypes["Library"] = ResolversParentTypes["Library"]
-> = {
-  address?: Resolver<Maybe<ResolversTypes["String"]>, ParentType, ContextType>
-  books?: Resolver<Maybe<Array<Maybe<ResolversTypes["Book"]>>>, ParentType, ContextType>
-  id?: Resolver<ResolversTypes["String"], ParentType, ContextType>
-  name?: Resolver<Maybe<ResolversTypes["String"]>, ParentType, ContextType>
-  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>
-}
+export type LibraryResolvers<ContextType = IContext, ParentType extends ResolversParentTypes['Library'] = ResolversParentTypes['Library']> = {
+  address?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  books?: Resolver<Maybe<Array<Maybe<ResolversTypes['Book']>>>, ParentType, ContextType>;
+  id?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  name?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
 
-export type QueryResolvers<
-  ContextType = IContext,
-  ParentType extends ResolversParentTypes["Query"] = ResolversParentTypes["Query"]
-> = {
-  author?: Resolver<Maybe<ResolversTypes["Author"]>, ParentType, ContextType, RequireFields<QueryAuthorArgs, "id">>
-  book?: Resolver<Maybe<ResolversTypes["Book"]>, ParentType, ContextType, RequireFields<QueryBookArgs, "id">>
-  books?: Resolver<Maybe<Array<Maybe<ResolversTypes["Book"]>>>, ParentType, ContextType>
-  library?: Resolver<Maybe<ResolversTypes["Library"]>, ParentType, ContextType, RequireFields<QueryLibraryArgs, "id">>
-}
+export type QueryResolvers<ContextType = IContext, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = {
+  author?: Resolver<Maybe<ResolversTypes['Author']>, ParentType, ContextType, RequireFields<QueryAuthorArgs, 'id'>>;
+  book?: Resolver<Maybe<ResolversTypes['Book']>, ParentType, ContextType, RequireFields<QueryBookArgs, 'id'>>;
+  books?: Resolver<Maybe<Array<Maybe<ResolversTypes['Book']>>>, ParentType, ContextType>;
+  library?: Resolver<Maybe<ResolversTypes['Library']>, ParentType, ContextType, RequireFields<QueryLibraryArgs, 'id'>>;
+};
 
 export type Resolvers<ContextType = IContext> = {
-  Author?: AuthorResolvers<ContextType>
-  Book?: BookResolvers<ContextType>
-  Editorial?: EditorialResolvers<ContextType>
-  Library?: LibraryResolvers<ContextType>
-  Query?: QueryResolvers<ContextType>
-}
+  Author?: AuthorResolvers<ContextType>;
+  Book?: BookResolvers<ContextType>;
+  Editorial?: EditorialResolvers<ContextType>;
+  Library?: LibraryResolvers<ContextType>;
+  Query?: QueryResolvers<ContextType>;
+};
+
